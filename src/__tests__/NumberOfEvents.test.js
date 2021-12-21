2da762c994d9c7e7b4cd5314c378661baa573787
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-12-21 17:48:55 -0500

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -11,15 +11,4 @@ describe('<NumberOfEvents unit testing', () => {
     test('render text input', () =>{
         expect(NumberOfEventsWrapper.find('.newValue')).toHaveLength(1);
     });
-
-    test("renders default number of 32 events", () => {
-        expect(NumberOfEventsWrapper.find('.events-number').prop("value")).toEqual(32);
-      });
-  
-    test("changes state when input changes", () => {
-      NumberOfEventsWrapper.setState({ numOfEvents: '32' });
-      const eventObject = { target: { value: '3' }};
-      NumberOfEventsWrapper.find(".events-number").simulate('change', eventObject);
-      expect(NumberOfEventsWrapper.state('numOfEvents')).toBe('3');
-      });
 })
