61a1d47ee3cbed208c8ec56ddb6c98f9c3089d0d
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-12-21 17:47:27 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
-import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-  
-  state = {
-    numberOfEvents: 32,
-    infoText: ''
-  }
-  
-  handleInputChanged = (event) => {
-    const number = event.target.value;
-    if (number < 1 || number > 100) {
-      this.setState({
-        infoText: 'Please choose between 1 and 100'
-      });
-    } else {
-      this.setState({ 
-        NumberOfEvents: number,
-        infoText:''
-      });
-      this.props.updateNumberOfEvents(number);
-    }
-  };
-
   render() {
     return (
       <div>
         <p>Change number of shown events</p>
         <input type="number" value={this.state.numberOfEvents} className="newValue" onChange={(e) => this.handleInputChanged(e)} />
-        <ErrorAlert text={this.state.infoText} />
       </div>
     );
   }
