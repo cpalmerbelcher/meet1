862b0d2f73dbdd14fb80f52e26315624f431bf4f
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-01-17 14:33:18 -0500

    adding testing
    adding Atatus to index.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('27827ffe2fd4472d8ac012d1f6060a14').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
