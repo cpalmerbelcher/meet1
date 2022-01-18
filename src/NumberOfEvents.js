bb6ed2b22b67543f42481579803669eb26f7cd1b
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-01-17 21:35:15 -0500

    Set up Alerts
    added infoAlerts and ErrorAlerts to my search bars

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    errorText: '',
   }
   
   handleInputChanged = (event) => {
     const number = event.target.value;
-    if (number < 1 || number > 100) {
+    if (number < 1 || number > 32) {
       this.setState({
-        infoText: 'Please choose between 1 and 100'
-      });
+        numberOfEvents: '',
+        errorText: 'Please choose between 1 and 32',
+      })
     } else {
       this.setState({ 
-        numberOfEvents: number
+        numberOfEvents: number,
+        errorText: '',
       });
-      this.props.updateNumberOfEvents(number);
     }
+    this.props.updateNumberOfEvents(event.target.value);
   };
   
   render() {
     return (
       <div>
-        <p>Change number of shown events</p>
+        <p>Number of Events Shown:</p>
         <input id="number-of-events__input" type="number" value={this.state.numberOfEvents} className="newValue" onChange={(e) => this.handleInputChanged(e)} />
+        <ErrorAlert text={this.state.ErrorAlert} />
       </div>
     );
   }
