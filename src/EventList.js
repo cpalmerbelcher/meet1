1c1955f8d702c90ca874c6d501aef65720265f7b
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-01-20 18:52:29 -0500

    added warning for when offline

--- src/EventList.js
+++ src/EventList.js
@@ -1,19 +1,36 @@
-import React, { Component } from "react";
-import Event from "./Event";
+import React, { Component } from 'react';
+import Event from './Event';
+import { Row, Col, Container } from 'react-bootstrap';
+import './EventList.css';
+import { WarningAlert } from './Alert';
 
 class EventList extends Component {
-  render() {
-    const { events } = this.props;
-    return (
-      <ul className="EventList">
-        {events && events.map(event => 
-          <li key={event.id} className="event">
-            <Event event={event} />
-          </li>
-        )}
-      </ul>
-    );
-  }
+	render() {
+		const { events } = this.props;
+		return (
+			<Container className="eventlist-container">
+				<Row className="d-flex justify-content-center event-list">
+					{!navigator.onLine ? (
+						<WarningAlert text="You are offline, the events list has been loaded from the Cache!" />
+					) : (
+						''
+					)}
+					<Col md={10} sm={12}>
+						<ul className="EventList row">
+							{events.map(event => (
+								<li
+									key={event.id}
+									className="col-xl-4 col-lg-4 col-md-8 col-sm-8 col-xs-12"
+								>
+									<Event event={event} />
+								</li>
+							))}
+						</ul>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default EventList;
